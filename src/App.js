5797f8526cbe739fc8f483cb63824b98b6d48f9f
jaeseong <qkrwotjd1445@naver.com>
2022-04-18 15:45:11 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Hello from "./Hello";
 
 function App() {
+const name = 'react';
+const style = {
+  backgroundColor: 'black',
+  color: 'aqua',
+  fontSize: 24, // 기본 단위 px
+  padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+}
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </>
   );
 }
 
